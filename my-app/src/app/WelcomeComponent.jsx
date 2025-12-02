14fc6752fb409d56bdc41eaeb647850d93f88dda
Julie <juth0001@stud.kea.dk>
2025-12-02 13:13:24 +0100

    welcome component sat ind på home

---
+++ my-app/src/app/WelcomeComponent.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+const WelcomeComponent = () =>{
+    return(
+<div>
+    
+</div>
+
+    );
+};
+export default WelcomeComponent;
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,10 +1,12 @@
 import './globals.css';
+import WelcomeCoomponent from '@/app/WelcomeComponent';
 
 export default function Home() {
   return (
     
       <main className="">
        <div className="">
+        <WelcomeCoomponent/>
 
         
        </div>
