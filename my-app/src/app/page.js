36ce3233a33893add492e81b4ce60b23b07648b1
oskarmmd <osde0001@stud.kea.dk>
2025-12-03 09:04:32 +0100

    Header

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
+
 export default function Home() {
   return (
     <div className="">
       <main className="">
-       
+       <Header />
       </main>
     </div>
   );
