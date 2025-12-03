f589b8997a33ef681c3ea3fc8ed19c45eb0c665f
Julie <juth0001@stud.kea.dk>
2025-12-03 12:47:12 +0100

    welcome component done

--- my-app/src/app/WelcomeComponent.jsx
+++ my-app/src/app/WelcomeComponent.jsx
@@ -1,4 +1,23 @@
 "use client";
+import './globals.css';
+import  WelcomeCard from "./WelcomeCard";
+
+export default function WelcomeComponent() {
+    return (
+        <main className='flex flex-col justify-center items-center'>
+          <h1 className='my-12 text-center'>
+            WELCOME IN NIGHT
+            <span className='text-(--pink)'>CLUB</span>
+          </h1>
+          <WelcomeCard />
+        </main>
+      );
+}
+
+
+
+
+/*"use client";
 import { useState, useEffect } from 'react';
 import './globals.css';
 import Image from 'next/image';
@@ -41,4 +60,4 @@ fetch("")
 
     );
 };
-export default WelcomeComponent;
+export default WelcomeComponent;*/
